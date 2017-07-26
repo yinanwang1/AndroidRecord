5840a47027243ece0c3414b544d7d4594abf2792
ArthurWang <wangyn@qeebike.com>
2017-07-26 10:00:17 +0800

    Test the JNI function is successful.

---
+++ HelloWorld/app/src/main/java/com/example/arthurwang/helloworld/July/JNIActivity.java
@@ -0,0 +1,23 @@
+package com.example.arthurwang.helloworld.July;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.arthurwang.helloworld.R;
+
+public class JNIActivity extends Activity {
+
+    protected TextView mTvJni;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jni);
+
+        mTvJni = (TextView) findViewById(R.id.tv_jni);
+
+        mTvJni.setText(new MyNdk().getString());
+    }
+
+}
