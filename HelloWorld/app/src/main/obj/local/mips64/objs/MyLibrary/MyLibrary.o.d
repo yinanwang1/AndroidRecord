249902e525199a57c074740f69efa23bb32b361b
ArthurWang <wangyn@qeebike.com>
2017-08-02 10:44:54 +0800

    Deleted the unused files.

--- HelloWorld/app/src/main/obj/local/mips64/objs/MyLibrary/MyLibrary.o.d
+++
@@ -1,6 +0,0 @@
-./obj/local/mips64/objs/MyLibrary/MyLibrary.o: jni/MyLibrary.c \
-  jni/com_example_arthurwang_helloworld_July_MyNdk.h jni/static.h
-
-jni/com_example_arthurwang_helloworld_July_MyNdk.h:
-
-jni/static.h:
