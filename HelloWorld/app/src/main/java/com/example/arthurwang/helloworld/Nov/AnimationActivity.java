94c740d68f930953e22e9525d6943fba382d0cbb
ArthurWang <wangyn@qeebike.com>
2018-12-07 17:38:58 +0800

    Update

--- HelloWorld/app/src/main/java/com/example/arthurwang/helloworld/Nov/AnimationActivity.java
+++ HelloWorld/app/src/main/java/com/example/arthurwang/helloworld/Nov/AnimationActivity.java
@@ -1,15 +1,125 @@
 package com.example.arthurwang.helloworld.nov;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import com.example.arthurwang.helloworld.R;
 
 public class AnimationActivity extends AppCompatActivity {
 
+    private static final String TAG = "AnimationActivity";
+
+    private Button mCreateWindowButton;
+
+    private Button mFloatingButton;
+    private WindowManager.LayoutParams mLayoutParams;
+    private WindowManager mWindowManager;
+
+    private boolean mHasPermission;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
+
+        mCreateWindowButton = findViewById(R.id.button1);
+        mWindowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+
+        requestDrawOverLays();
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void requestDrawOverLays() {
+        if (!Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+            intent.setData(Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, 0);
+
+            mHasPermission = false;
+        } else {
+            mHasPermission = true;
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 0) {
+            mHasPermission = Settings.canDrawOverlays(this);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+
+
+
+    public void onButtonClick(View v) {
+
+            if (!mHasPermission) {
+                requestDrawOverLays();
+
+                return;
+            }
+
+
+
+
+        if (v == mCreateWindowButton) {
+            mFloatingButton = new Button(this);
+            mFloatingButton.setText("就要与众不同");
+
+            mLayoutParams = new WindowManager.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    0,
+                    0,
+                    PixelFormat.TRANSPARENT);
+            mLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+            mLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                    | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                    | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED;
+            mLayoutParams.gravity = Gravity.LEFT | Gravity.TOP;
+            mLayoutParams.x = 100;
+            mLayoutParams.y = 300;
+
+            mWindowManager.addView(mFloatingButton, mLayoutParams);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int rawX = (int) event.getRawX();
+        int rawY = (int) event.getRawY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:{
+                mLayoutParams.x = rawX;
+                mLayoutParams.y = rawY;
+
+                mWindowManager.updateViewLayout(mFloatingButton, mLayoutParams);
+
+
+                break;
+            }
+
+            default:
+                break;
+        }
+
+
+        return false;
     }
 }
