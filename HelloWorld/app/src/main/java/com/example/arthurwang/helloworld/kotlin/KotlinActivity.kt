7886f9f8a475d8c64cd8ad1be9f961fb5d6ede4c
ArthurWang <wangyn@qeebike.com>
2019-02-14 15:37:01 +0800

    update

--- HelloWorld/app/src/main/java/com/example/arthurwang/helloworld/kotlin/KotlinActivity.kt
+++ HelloWorld/app/src/main/java/com/example/arthurwang/helloworld/kotlin/KotlinActivity.kt
@@ -1,8 +1,11 @@
 package com.example.arthurwang.helloworld.kotlin
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 import com.example.arthurwang.helloworld.R
+import java.io.BufferedReader
 
 class KotlinActivity : AppCompatActivity() {
 
@@ -87,9 +90,86 @@ class KotlinActivity : AppCompatActivity() {
 //        val numbersTo100 = naturalNumbers.takeWhile { it <= 100 }
 //        println(numbersTo100.sum())
 
-        println("alphabet is ${alphabet()}")
+//        println("alphabet is ${alphabet()}")
+
+//        println("createViewWithCustomerAttributes is ${createViewWithCustomerAttributes(this)}")
+//
+//        ll_test.addView(createViewWithCustomerAttributes(this))
+
+//        var email: String? = "yole@example.com"
+//        email?.let { sendEmailTo(it) }
+//
+//        email = null
+//        email?.let { sendEmailTo(it) }
+
+//        val p1 = Point(10, 20)
+//        val p2 = Point(30, 40)
+//        println("wyn ${p1 + p2}")
+
+//        println('a' * 3)
+
+//        var point = Point(1, 2)
+//        point += Point(3, 4)
+//        println("point is $point")
+
+//        val list = arrayListOf(1, 2)
+//        list += 3
+//        val newList = list + listOf(4, 5)
+//        println(list)
+//        println(newList)
+
+//        twoAndThree{a, b -> a + b}
+//        twoAndThree{a, b -> a * b}
+
+//        println("ab1c".filter { it in 'a'..'z' })
+
+        val calculator = getShippingCostCalculator(Delivery.EXPEDITED)
+        println("Shipping costs ${calculator(Order(3))}")
+
+    }
+
+    enum class Delivery {STANDARD, EXPEDITED}
+
+    class Order(val itemCount: Int)
+
+    fun getShippingCostCalculator(delivery: Delivery) : (Order) -> Double {
+        if (delivery == Delivery.EXPEDITED) {
+            return {order -> 6 + 2.1 * order.itemCount }
+        }
+
+        return {order -> 1.2 * order.itemCount }
+    }
+
+    fun twoAndThree(operation: (Int, Int) -> Int) {
+        val result = operation(2, 3)
+        println("The result is $result")
+    }
 
 
+
+    operator fun Char.times(count: Int): String {
+        return toString().repeat(count)
+    }
+
+    data class Point(val x: Int, val y: Int) {
+        operator fun plus(other: Point): Point {
+            return Point(x + other.x, y + other.y)
+        }
+    }
+
+    fun readNumbers(reader: BufferedReader) : List<Int?> {
+        val result = ArrayList<Int?>()
+        for (line in reader.lineSequence()) {
+            val number = line.toIntOrNull()
+            result.add(number)
+        }
+
+        return result
+    }
+
+
+    fun sendEmailTo(email: String) {
+        println("Sending email to $email")
     }
 
     data class Person(val name: String, val age: Int = 0)
@@ -112,6 +192,12 @@ class KotlinActivity : AppCompatActivity() {
         result.append("\nNow I know the alphabet!")
 
         return result.toString()
+    }
+
+    fun createViewWithCustomerAttributes(context: Context) = TextView(context).apply {
+        text = "Sample Text"
+        textSize = 20.0f
+        setPadding(10, 0, 0, 0)
     }
 
 
