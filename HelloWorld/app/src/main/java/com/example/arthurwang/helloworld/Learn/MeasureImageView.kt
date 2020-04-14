83af21d8a3f349c4c7f4a64b9d483fc869fbdf47
ArthurWang <wangyn@qeebike.com>
2020-04-14 19:56:48 +0800

    Update

--- HelloWorld/app/src/main/java/com/example/arthurwang/helloworld/Learn/MeasureImageView.kt
+++ HelloWorld/app/src/main/java/com/example/arthurwang/helloworld/Learn/MeasureImageView.kt
@@ -2,20 +2,39 @@ package com.example.arthurwang.helloworld.Learn
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
+import com.socks.library.KLog
 
 /**
  * Created by arthurwang on 2020/4/10
  */
 class MeasureImageView(context: Context, attributeSet: AttributeSet): AppCompatImageView(context, attributeSet) {
 
+
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+//        KLog.e("wyn", "widthMeasureSpec is $widthMeasureSpec")
+//        KLog.e("wyn", "heightMeasureSpec is $heightMeasureSpec")
+//
+//
+//        setMeasuredDimension(View.getDefaultSize(0, widthMeasureSpec), View.getDefaultSize(0, heightMeasureSpec))
+//
+//        val childWidthSize = measuredWidth
+//        val widthTemp = MeasureSpec.makeMeasureSpec(childWidthSize, MeasureSpec.EXACTLY)
+//        val heightTemp = widthTemp
+//
+//        KLog.e("wyn", "heightTemp is $heightTemp")
+//        KLog.e("wyn", "heightTemp is $heightTemp")
 
-
-
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
 
     }
 
 
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+
+
+    }
 }
