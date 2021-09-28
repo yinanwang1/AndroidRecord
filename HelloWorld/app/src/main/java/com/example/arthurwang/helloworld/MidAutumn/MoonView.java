d30cd10cbfbf08685f9b8797a31486b360a0250a
ArthurWang <wangyn@qeek.cc>
2021-09-28 10:23:37 +0800

    Update

--- HelloWorld/app/src/main/java/com/example/arthurwang/helloworld/MidAutumn/MoonView.java
+++ HelloWorld/app/src/main/java/com/example/arthurwang/helloworld/MidAutumn/MoonView.java
@@ -11,15 +11,6 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 import com.example.arthurwang.helloworld.R;
-import com.example.arthurwang.helloworld.base.KLog.KLog;
-
-import java.util.concurrent.TimeUnit;
-
-import io.reactivex.Observable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by arthurwang on 2021/9/18
@@ -27,10 +18,6 @@ import io.reactivex.schedulers.Schedulers;
 public class MoonView extends View {
     private Paint paint;
     private Bitmap moon;
-    private int x = 0;
-    private int y = 100;
-    private int a = 5;
-    private Disposable disposable;
 
     public MoonView(Context context) {
         this(context, null);
@@ -48,32 +35,13 @@ public class MoonView extends View {
 
         moon = BitmapFactory.decodeResource(getResources(), R.drawable.moon);
 
-        disposable = Observable.interval(1000, TimeUnit.MILLISECONDS)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<Long>() {
-                    @Override
-                    public void accept(Long aLong) throws Exception {
-                        x += 10;
-                        y = (8 * a * a * a) / (x * x + 4 * a * a) + 100;
-
-                        invalidate();
-
-                        KLog.e("wyn", "111");
-
-                    }
-                });
-
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawBitmap(moon, x, y, paint);
+        canvas.drawBitmap(moon, 100, 200, paint);
 
-        if (x > getWidth()) {
-            disposable.dispose();
-        }
     }
 }
